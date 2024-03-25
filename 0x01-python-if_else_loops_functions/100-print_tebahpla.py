b15129e99575d72398c3c1dd65a9297e90963c21
edmuhumuza <muhumuzaedgar19@gmail.com>
2024-03-25 06:46:39 +0000

    Check code correctness

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-x = 1
+x=1
 for _ in range(122, 96, -1):
     if x % 2 == 0:
-        print("{:c}".format(_-32), end = "")
+        print("{:c}".format(_-32), end="")
         x = x + 1
     else:
-        print("{:c}".format(_), end = "")
+        print("{:c}".format(_), end="")
         x = x + 1
