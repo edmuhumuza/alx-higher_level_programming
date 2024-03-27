df8cc3976125f4e0e9a1e59518e2ee62b950463c
edmuhumuza <muhumuzaedgar19@gmail.com>
2024-03-27 10:45:49 +0000

    Check code correctness

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,13 +5,13 @@ if __name__ == '__main__':
         print('0 arguments.')
     else:
         if len(sys.argv) == 2:
-            print('{} argument:'.format(len(sys.argv)-1))
-            print('{}: {}'.format(len(sys.argv)-1, sys.argv[1]))
+            print('{} argument:'.format(len(sys.argv) - 1))
+            print('{}: {}'.format(len(sys.argv) - 1, sys.argv[1]))
         elif len(sys.argv) > 1:
             print('{} arguments:'.format(len(sys.argv) - 1))
             x = 1
             for _ in sys.argv:
-                if  _ == sys.argv[0]:
+                if _ == sys.argv[0]:
                     continue
                 print('{}: {}'.format(x, sys.argv[x]))
                 x += 1
