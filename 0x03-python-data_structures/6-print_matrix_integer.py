b2d9f207fc3bb1e8d53ec9e9ee3c2a6cbc768d30
edmuhumuza <muhumuzaedgar19@gmail.com>
2024-04-05 09:57:37 +0000

    Check code correctness

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if len(matrix) == 0:
-        print()
-    else:
+    if matrix:
         for list in matrix:
             for digit in list:
                 if digit != list[-1]:
                     print("{:d} ".format(digit), end='')
                 else: 
                     print("{:d}".format(digit))
+    else:
+        print()
