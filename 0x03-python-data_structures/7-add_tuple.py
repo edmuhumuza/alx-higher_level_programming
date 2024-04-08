f5a7e97e0939098265f06b157c0bc33f1edbfe1c
edmuhumuza <muhumuzaedgar19@gmail.com>
2024-04-08 14:56:48 +0000

    Check code correctness

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) == 0 and len(tuple_b) == 0:
+        return ()
+    if len(tuple_a) == 1 and len(tuple_b) == 1:
+        return tuple_a[0] + 0, tuple_b[0] + 0
     if len(tuple_a) == 0:
         return tuple_b
     if len(tuple_b) == 0:
