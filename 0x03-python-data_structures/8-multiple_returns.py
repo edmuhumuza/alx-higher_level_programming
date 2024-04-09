4ca599848b5918a5a86ebb99410d2c040b3f8eca
edmuhumuza <muhumuzaedgar19@gmail.com>
2024-04-09 09:35:05 +0000

    Check code

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        return(0, None)
+        return (0, None)
     length = 0
     for _ in sentence:
-        length += length
-    return(length, sentence[0])
+        length = length + 1
+    return (length, sentence[0])
