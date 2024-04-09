10c7af3070fe3d2930016595149f38173e599525
edmuhumuza <muhumuzaedgar19@gmail.com>
2024-04-09 07:49:24 +0000

    Check code correctness

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -14,3 +14,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
         return tuple_a[0] + tuple_b[0], tuple_a[1] + 0
     if len(tuple_a) >= 2 and len(tuple_b) >= 2:
         return tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
+
