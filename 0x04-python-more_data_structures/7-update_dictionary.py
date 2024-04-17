e78e8ae165ad8b42318ead1b6115068c7b3155a9
edmuhumuza <muhumuzaedgar19@gmail.com>
2024-04-17 11:53:43 +0000

    Check code

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if key in a_dict:
-        a_dict[key] = value
+    if a_dicitionary[key]:
+        a_dictionary[key] = value
     else:
-        a_dict[key] = value
+        a_dictionary[key] = value
