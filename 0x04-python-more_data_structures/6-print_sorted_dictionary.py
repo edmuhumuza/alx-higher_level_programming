e7ab3458fadfb5c560360107663f2a343c57217b
edmuhumuza <muhumuzaedgar19@gmail.com>
2024-04-17 11:30:52 +0000

    Check code

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    for llave in sorted(a_dictionary):
-        print("{}: {}".format(llave, a_dictionary[llave]))
+    a_keys = sorted(a_dictionary.keys())
+    for a_key in a_keys:
+        print("{}: {}".format(a_key, a_dictionary[a_key]))
