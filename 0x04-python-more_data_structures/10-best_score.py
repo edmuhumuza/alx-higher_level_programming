ae8d1bcf327c3310d8aa638747eb596f984ec5cc
edmuhumuza <muhumuzaedgar19@gmail.com>
2024-04-19 12:10:27 +0000

    Check code

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
-def best_score(my_dict):
-    if my_dict is None or my_dict == {}:
+def best_score(a_dictionary):
+    if not a_dictionary:
         return None
-    biggest = max(my_dict.values())
-    for key, value in my_dict.items():
-        if value is biggest:
-            return key
+    d_values = list(a_dictionary.values())
+    n = len(d_values)
+    for value in range(n):
+        for x in range(n-value-1):
+            if d_values[x] > d_values[x+1]:
+                d_values[x], d_values[x+1] = d_values[x+1], d_values[x]
+    g_value = d_values[-1]
+
+    d_keys = a_dictionary.keys()
+    for d_key in d_keys:
+        if a_dictionary[d_key] == g_value:
+            return d_key
