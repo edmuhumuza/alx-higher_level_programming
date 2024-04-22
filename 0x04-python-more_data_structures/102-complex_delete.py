fce418e152779c9d2809867d143b343356768580
edmuhumuza <muhumuzaedgar19@gmail.com>
2024-04-22 12:18:50 +0000

    Check code

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,9 +2,9 @@
 def complex_delete(a_dictionary, value):
     if value not in a_dictionary.values():
         return a_dictionary
-    while (value in a_dictionary.values()):
+    while(value in a_dictionary.values()):
         new_dict = dict(a_dictionary)
-        for k,v in new_dict.items():
+        for k, v in new_dict.items():
             if new_dict[k] == value:
                 del a_dictionary[k]
-        return a_dictionary
+    return a_dictionary
