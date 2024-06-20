9cf88621a5fdd068a5baa65b9d8174fb55e84ebe
edmuhumuza <muhumuzaedgar19@mail.com>
2024-06-20 11:25:55 -0700

    Second biggest!

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const y = 0;
+if (process.argv.length <= 2) {
+  console.log(y);
+} else if (process.argv.length === 3) {
+  console.log(y);
+} else {
+  const array = [];
+  for (let x = 2; x <= process.argv.length; x++) {
+    if (Number(process.argv[x])) {
+      array.push(Number(process.argv[x]));
+    }
+  }
+  const newArray = array.sort();
+  console.log(newArray[newArray.length - 2]);
+}
